26cf79cd1f6ccd1165bea991cc1ec1803b11d86d
Vince Skahan <vinceskahan@gmail.com>
2023-11-03 19:28:48 -0700

    quick refactor with minimal test script

---
+++ deb11pip/test.sh
@@ -0,0 +1,12 @@
+
+# test.sh script
+
+echo "#----------------------"
+echo "this is the test script"
+echo "#----------------------"
+echo "run test 1"
+echo "run test 2"
+echo "exiting 123 to test return status..."
+
+exit 123
+
